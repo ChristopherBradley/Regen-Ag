09d29092173cf38168efd582ed32a6df8ac25dbb
Christopher Bradley <chris2bradley@gmail.com>
2024-07-25 16:29:52 +1000

    Density plot ACT Trees vs Global Canopy height

--- Google_Earth_Engine/ACT Crown vs Global Canopy Height.py
+++ Google_Earth_Engine/ACT Crown vs Global Canopy Height.py
@@ -1,63 +1,41 @@
-# +
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import rasterio
+from rasterio.mask import mask
 import geopandas as gpd
 import matplotlib.pyplot as plt
-
-from rasterio.mask import mask
 from shapely.geometry import box
 from scipy.stats import gaussian_kde
-# -
-
+from datetime import datetime
 
 # Read the GeoPackage
+start = datetime.now()
 gdf = gpd.read_file("ACTGOV_Mature_Trees_2020_sppID_singles.gpkg")
-gdf_crs = gdf.crs
 
-gdf.crs
-
-# Open the TIFF file and get its bounding box
+# Open the Global Canopy Height TIFF file and get its bounding box
 with rasterio.open("Canberra tiles/311230302.tif") as src:
     bbox = box(*src.bounds)
     canopy_height_data = src.read(1)  # Read the first band
     canopy_height_meta = src.meta
 canopy_height_meta
 
+# Clip the ACT Tree Crowns to the area within this Global Canopy Height bbox
 raster_crs = src.meta['crs']
-gdf_crs = gdf.crs
-
-print(raster_crs)
-print(gdf_crs)
-
-print(type(raster_crs))
-print(type(gdf_crs))
-
 gdf = gdf.to_crs(raster_crs)
-print("GeoPackage reprojected to match raster CRS.")
-
 gdf_clipped = gdf[gdf.intersects(bbox)]
 print(f"Number of tree crowns within raster bounds: {len(gdf_clipped)}")
 
+# Using a subset of 20k trees to speed things up
+gdf_clipped = gdf_clipped[:20000]
 
-# Function to get the maximum height within a polygon
 def get_max_height(polygon, src):
-    # Mask the raster with the polygon
-    out_image, out_transform = mask(src, [polygon], crop=True)
-    
-    # Get the data array within the mask
+    """Find the maximum height within the polygon"""
+    out_image, _ = mask(src, [polygon], crop=True)
     data = out_image[0]
-    
-    # Calculate the maximum value, ignoring nodata values
     max_height = np.nanmax(data[data != src.nodata])
-    
     return max_height
 
-
-len(gdf_clipped)
-
-# Store results
+# Find the maximum global canopy height within each ACT Tree Crown
 results = []
 with rasterio.open("Canberra tiles/311230302.tif") as src:
     for index, row in gdf_clipped.iterrows():
@@ -68,13 +46,12 @@ with rasterio.open("Canberra tiles/311230302.tif") as src:
             'max_height_geopackage': row['max_height'],
             'max_height_tiff': max_canopy_height
         })
-
 results_df = pd.DataFrame(results)
 
+# Density plot to compare the two datasets
 nbins = 300
-
-x = results_df['max_height_geopackage'] # change 'x' with your column name
-y = results_df['max_height_tiff'] # change 'y' with your column name
+x = results_df['max_height_geopackage']
+y = results_df['max_height_tiff']
 
 k = gaussian_kde([x,y])
 xi, yi = np.mgrid[
@@ -86,56 +63,11 @@ zi = k(np.vstack([
    yi.flatten()
 ])).reshape(xi.shape)
 
-
 fig, ax = plt.subplots(figsize=(8,8))
 ax.pcolormesh(xi, yi, zi)
+ax.set_xlabel('ACT Tree Crowns', fontsize=12)
+ax.set_ylabel('Global Canopy Height', fontsize=12)
 plt.show()
 
-
-
-
-# Extract the data
-# x = results_df['max_height_geopackage']
-# y = results_df['max_height_tiff']
-
-# Calculate the point density
-# xy = np.vstack([x, y])
-# z = gaussian_kde(xy)(xy)
-
-# # Sort the points by density, so that the densest points are plotted last
-# idx = z.argsort()
-# x, y, z = x[idx], y[idx], z[idx]
-
-# # Create the scatter plot
-# plt.figure(figsize=(10, 6))
-# plt.scatter(x, y, c=z, s=50, edgecolor='black', cmap='viridis')
-# plt.colorbar(label='Density')
-# plt.xlabel('Max Height GeoPackage')
-# plt.ylabel('Max Height TIFF')
-# plt.title('Scatter Plot of Max Heights Colored by Density')
-# plt.grid(True)
-# plt.show()
-
-# sns.pairplot(results_df, vars=['max_height_geopackage', 'max_height_tiff'], plot_kws={'alpha': 0.3})
-# plt.show()
-
-
-# +
-# # Define the height bins and labels
-# bins = [0, 3, 5, 10, 15, 20, float('inf')]
-# labels = ['0-3', '3-5', '5-10', '10-15', '15-20', '>20']
-
-# # Create a new column for the height categories
-# results_df['height_bin'] = pd.cut(results_df['max_height_geopackage'], bins=bins, labels=labels, right=False)
-
-# # -
-
-# plt.figure(figsize=(12, 8))
-# sns.boxplot(x='height_bin', y='max_height_tiff', data=results_df)
-# plt.xlabel('Max Height GeoPackage (bins)')
-# plt.ylabel('Max Height TIFF')
-# plt.title('Boxplot of Max Height TIFF for Different Height Ranges of GeoPackage')
-# plt.grid(True)
-# plt.show()
-
-print()
+end = datetime.now()
+print("Time taken: ", end-start)
